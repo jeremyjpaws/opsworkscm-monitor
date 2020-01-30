575ac638f7d114a7a768fc93500d2194558a0930
EC2 Default User <ec2-user@ip-172-64-1-199.ec2.internal>
2020-01-30 22:40:15 +0000

    minor comment updates

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,8 +4,8 @@
 #
 # The intended use is to invoke the Lambda from a periodic CloudWatch
 # cron-expression rule. Then this Lambda describes the given server
-# status. The status is checked if it is UNHEALTHY, and if so,
-# an SNS notification is generated.
+# status. The status is checked if it is the given STATUS, 
+# such as UNHEALTHY, and if so, an SNS notification is generated.
 #
 # Note this code is provided as-is, as an example only. It has not been
 # tested with any rigor. Please test and modify as you need, before
